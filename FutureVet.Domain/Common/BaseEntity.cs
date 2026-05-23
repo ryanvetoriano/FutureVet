58649665cb2985af9de5817dbcc34368caa88491
ryanvetoriano <vetorianosilva@gmail.com>
2026-05-23 15:49:24 -0300

    Init: Começando o projeto e criação de BaseEntity.cs

---
+++ FutureVet.Domain/Common/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿namespace FutureVet.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+        = Guid.NewGuid();
+
+    public bool Disponivel { get; private set; }
+        = true;
+
+    public DateTime DataCriacao { get; private set; }
+        = DateTime.UtcNow;
+
+    public DateTime? DataAtualizacao { get; private set; }
+
+    public void Activate()
+    {
+        Disponivel = true;
+        AtualizarData();
+    }
+
+    public void Deactivate()
+    {
+        Disponivel = false;
+        AtualizarData();
+    }
+
+    protected void AtualizarData()
+    {
+        DataAtualizacao = DateTime.UtcNow;
+    }
+}
