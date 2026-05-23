fdb395d74e8c85eeebffc785db770b6bab06fc37
ryanvetoriano <vetorianosilva@gmail.com>
2026-05-23 15:51:28 -0300

    feat: Criação dos dois Enums que serão usados no projeto

---
+++ FutureVet.Domain/Enums/EspeciePet.cs
@@ -0,0 +1,9 @@
+﻿namespace FutureVet.Domain.Enums;
+
+public enum EspeciePet
+{
+    Cao = 1,
+    Gato = 2,
+    Coelho = 3,
+    Outro = 4
+}
---
+++ FutureVet.Domain/Enums/PortePet.cs
@@ -0,0 +1,8 @@
+﻿namespace FutureVet.Domain.Enums;
+
+public enum PortePet
+{
+    Pequeno = 1,
+    Medio = 2,
+    Grande = 3
+}
